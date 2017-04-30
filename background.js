e39d13e05f7f278bc3782ce6bb4e70dac223213a
Ivan Novikov <ivan7237d@gmail.com>
2017-04-30 11:00:09 +0300

    Initial commit

--- background.js
+++ background.js
@@ -1,5 +1,28 @@
 'use strict';
 
-let disableShelf = () => chrome.downloads.setShelfEnabled(false);
-chrome.runtime.onInstalled.addListener(disableShelf);
-chrome.runtime.onStartup.addListener(disableShelf);
+// See https://github.com/tfoxy/chrome-promise
+chrome.promise = new ChromePromise();
+
+chrome.commands.onCommand.addListener(async command => {
+  const incr = (() => {
+    if (command == 'move_left') {
+      return -1;
+    }
+    if (command == 'move_right') {
+      return 1;
+    };
+  })();
+  const [currentWindow, highlightedTabs] = await Promise.all([
+    chrome.promise.windows.getCurrent({populate: true}),
+    chrome.promise.tabs.query({currentWindow: true, highlighted: true}),
+  ]);
+  const tabToMove = highlightedTabs[0];
+  const moveTo = (() => {
+    if (incr == 1 && tabToMove.index == currentWindow.tabs.length - 1) {
+      return 0;
+    } else {
+      return tabToMove.index + incr;
+    }
+  })();
+  chrome.promise.tabs.move(tabToMove.id, {index: moveTo});
+});
